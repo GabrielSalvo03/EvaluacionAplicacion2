090565c01086525c266c78ea5c492a867635fb1b
felipe <pipevaldes05@gmail.com>
2024-10-27 23:23:50 -0300

    Initial commit

---
+++ app/src/main/java/com/example/evaluacion2/entrenamiento.kt
@@ -0,0 +1,11 @@
+package com.example.evaluacion2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EntrenamientoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entrenamiento)
+    }
+}
